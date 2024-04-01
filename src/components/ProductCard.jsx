28b092c325952248947a78b9de231ad022bba58c
Dalton <daltongetbaker@gmail.com>
2024-03-31 20:47:58 -0400

    Started product card sun

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,39 @@
-import { Link } from "react-router-dom";
 
-// We are deconstructing props object directly in the parentheses of the function
-function ProjectCard ( { title, description, _id } ) {
-  
-  return (
-    <div className="ProjectCard card">
-      <Link to={`/projects/${_id}`}>
-        <h3>{title}</h3>
-      </Link>
-      <p style={{ maxWidth: "400px" }}>{description} </p>
-    </div>
-  );
+import { Link } from 'react-router-dom'
+import { useContext, useEffect } from "react";
+
+
+
+function ProductCard ( { title, price, _id, handleDelete } ) {
+
+    return (
+        <div className="product-card">
+            <div className="product-image">
+                <img src={thumbnail} alt={image} />
+            </div>
+
+            <div className="price">{price}</div>
+
+            <div className='product-btns'>
+                <button onClick={() => handleDelete(_id)}>Delete</button>
+                <Link to = {`/products/${_id}`}>
+                    <button>Details</button>           
+                </Link>
+            </div>
+        </div>
+    )
+
+    
 }
 
-export default ProjectCard;
+export default ProductCard;
+
+// title: String,
+// description: String,
+// price: Number,
+// rating: Number,
+// stock: Number,
+// brand: String,
+// category: String,
+// thumbnail: String,
+// images: Array,
