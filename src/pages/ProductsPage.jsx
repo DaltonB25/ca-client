8f9e18c9c17846e850833a9d13e35ffa73817498
Dalton <daltongetbaker@gmail.com>
2024-03-31 20:48:19 -0400

    tried to get api working

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,36 +1,88 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-import { get } from "../services/authService";
+// import { get } from "../services/authService";
 
-import ProjectCard from "../components/ProjectCard";
-import AddProject from "../components/AddProject";
+import ProductCard from "../components/ProductCard";
 
+const API_URL = import.meta.env.VITE_API_URL;
 
+function ProductsPage() {
+  const [products, setProducts] = useState([]);
 
-function ProjectListPage() {
-  const [projects, setProjects] = useState([]);
+  const handleChange = (event, updateState) => {
+    updateState(event.target.value);
+  };
 
-  const getAllProjects = () => {
-    get('/projects')
-      .then((response) => setProjects(response.data))
+    axios
+      .get(`${API_URL}/api/products`)
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => console.log(error));
+  
+
+  const getAllProducts = () => {
+    axios
+      .get(`${API_URL}/api/products`)
+      .then((response) => {
+        setProducts(response.data);
+      })
       .catch((error) => console.log(error));
   };
 
-  // We set this effect will run only once, after the initial render
-  // by setting the empty dependency array - []
   useEffect(() => {
-    getAllProjects();
+    getAllProducts();
   }, []);
 
   return (
-    <div className="ProjectListPage">
-      <AddProject refreshProjects={getAllProjects} />
+    <div className="ProductPage">
+   
+      <div className="flex justify-between items-center p-2 font-bold border-b">
+        <span style={{ flexBasis: "25%" }}>product</span>
+        <span style={{ flexBasis: "25%" }}>Id</span>
+      </div>
 
-      {projects.map((project) => (
-        <ProjectCard key={project._id} {...project} />
-      ))}
-    </div>
+      {products &&
+        products.map(
+          (product) => (
+              <ProductCard
+                key={product._id}
+                {...product}
+                className={index % 2 === 0 ? "bg-white" : "bg-gray-100"}
+              />
+          )
+        )}
+  
+  </div>
   );
 }
 
-export default ProjectListPage;
+export default ProductsPage;
+// const getAllProducts = () => {
+//   axios
+//   .get(`${API_URL}/products`)
+//   console.log(response.data)
+//     .then((response) => setProducts(response.data))
+//     .catch((error) => console.log(error));
+// };
+
+// // We set this effect will run only once, after the initial render
+// // by setting the empty dependency array - []
+// useEffect(() => {
+//   getAllProducts();
+// }, []);
+
+// return (
+//   <div className="ProductPage">
+
+//     {products.map((product) => {
+//         return (
+//           <div className="ProductCard card" key={product.id} >
+//             <Link to={`/products/${product.id}`}>
+//               <h3>{product.title}</h3>
+//             </Link>
+//           </div>
+//         );
+//       })}     
