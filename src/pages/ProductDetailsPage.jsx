9d41f115d7c0eb2d9e4a12ed587fb07255d97751
Dalton <daltongetbaker@gmail.com>
2024-04-01 20:26:21 -0400

    4/1 finishing PDP

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -4,55 +4,53 @@ import { useParams, Link } from "react-router-dom";
 
 import { get } from "../services/authService";
 
-import AddTask from "../components/AddTask";
-import TaskCard from "../components/TaskCard";
+function ProductDetailsPage(props) {
+  const [product, setProduct] = useState(null);
+  const { productId } = useParams();
 
-function ProjectDetailsPage(props) {
-  const [project, setProject] = useState(null);
-  const { projectId } = useParams();
-
-  const { user } = useContext(AuthContext);
-
-  const getProject = () => {
-    get(`/projects/details/${projectId}`)
+  const getProduct = () => {
+    get(`/products/${productId}`)
       .then((response) => {
-        const oneProject = response.data;
-        setProject(oneProject);
+        setProduct(response.data)
+        // const oneProduct = response.data;
+        // setProduct(oneProduct);
       })
       .catch((error) => console.log(error));
   };
 
   useEffect(() => {
-    getProject();
+    getProduct();
   }, []);
 
   return (
-    <div className="ProjectDetails">
-      {project && (
-        <>
-          <h1>{project.title}</h1>
-          <p>{project.description}</p>
-        </>
-      )}
-
-      {user && project && user._id === project.owner && (
-        <AddTask refreshProject={getProject} projectId={projectId} />
-      )}
-
-      {project &&
-        project.tasks.map((task) => <TaskCard key={task._id} {...task} />)}
-
-      <Link to="/projects">
-        <button>Back to projects</button>
+    <div className="max-w-2xl mx-auto p-6">
+      <Link to="/products" className="absolute top-16 left-4 z-10">
+        <button className="bg-gray-600 hover:bg-gray-700 text-white font-bold mt-5 py-2 px-4 border border-black rounded-lg">
+          Back to Products
+        </button>
       </Link>
-
-      {user && project && user._id === project.owner && (
-        <Link to={`/projects/edit/${projectId}`}>
-          <button>Edit Project</button>
-        </Link>
+      {product && (
+        <div className="bg-white shadow-lg rounded-lg overflow-hidden relative mt-8 flex flex-row">
+          <img src={product.thumbnail} alt={product.title} className="w-full h-64  object-cover object-center" />
+          <div className="p-4 border-black border-2 border-solid rounded-lg ">
+            <h1 className="text-2xl font-bold mb-2">{product.title}</h1>
+            <p className="text-gray-700 mb-4">{product.description}</p>
+            <p className="text-md font-semibold mb-2">Price: ${product.price}</p>
+            <p className="text-md font-semibold mb-2">Rating: {product.rating}</p>
+            <p className="text-md font-semibold mb-2">Stock: <span className={product.stock ? 'text-green-500' : 'text-red-500'}>{product.stock ? 'In Stock' : 'Out of Stock'}</span></p>
+            <p className="text-md font-semibold mb-2">Brand: {product.brand}</p>
+            <p className="text-md font-semibold mb-8">Category: {product.category}</p>
+          </div>
+          <div className="absolute bottom-1 right-2">
+          <button className="bg-gray-600 hover:bg-gray-700 text-white font-bold mt-5 py-2 px-4 border border-black rounded-lg">
+              Add to Cart
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
 }
 
-export default ProjectDetailsPage;
+export default ProductDetailsPage;
+
