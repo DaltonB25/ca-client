ecd9ef26b9303bbfb7943077133fe92d11feaf6d
Dalton <daltongetbaker@gmail.com>
2024-04-02 22:30:50 -0400

    added cart to the navbar aswell as tailwind updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { useContext } from "react"; // <== IMPORT
 import { AuthContext } from "../context/auth.context"; // <== IMPORT
 
+import cartIcon from "../images/cart.png";
+
 function Navbar() {
   // Subscribe to the AuthContext to gain access to
   // the values from AuthContext.Provider `value` prop
@@ -15,40 +17,45 @@ function Navbar() {
   //  Update the rendering logic to display different content
   //  depending on whether the user is logged in or not
   return (
-    <nav className="bg-gray-600 fixed w-screen top-0 mb-4 h-16 flex justify-start p-5  ">
-      <Link to="/">
-        <button className="text-white pr-5">Home</button>
-      </Link>
+    <nav className="bg-gray-600 fixed w-screen top-0 mb-4 h-16 flex justify-between items-center p-5">
+      <div className="flex items-center">
+        <Link to="/">
+          <button className="text-white pr-5">Home</button>
+        </Link>
+        {getToken() && (
+          <Link to="/products">
+            <button className="text-white pr-5">Products</button>
+          </Link>
+        )}
+        {getToken() && (
+          <Link to="/users/:userId">
+            <button className="text-white pr-5">Profile</button>
+          </Link>
+        )}
+        {getToken() && (
+          <button className="text-white pr-5" onClick={logOutUser}>
+            Logout
+          </button>
+        )}
+      </div>
 
-      {/*    UPDATE     */}
-      {getToken() ? (
-        <>
-        <Link to="/products">
-          <button className="text-white pr-5">Products</button>
-        </Link>
-        <Link to="/carts/`${cartId}`">
-        <button className="text-white pr-5">Cart</button>
-        </Link>
-        <Link to="/users">
-        <button className="text-white pr-5">Profile</button>
-        </Link>
-          <button className="text-white pr-5" onClick={logOutUser}>Logout</button>
-          {/* <span>{user && user.name}</span> */}
-        </>
-      ) : (
-        <>
+        {getToken() ? (
+          <div className="relative">
+           <Link to="/carts/:cartId">
+            <img src={cartIcon} alt="Cart" className="h-8 w-8 mr-3" />
+           </Link>
+         </div>
+         ) : (
+        <div>
           <Link to="/signup">
-            {" "}
-            <button className="text-white pr-5">Sign Up</button>{" "}
+            <button className="text-white pr-5">Sign Up</button>
           </Link>
           <Link to="/login">
-            {" "}
-            <button className="text-white pr-5">Login</button>{" "}
+            <button className="text-white pr-5">Login</button>
           </Link>
-        </>
+        </div>
       )}
     </nav>
   );
 }
-
 export default Navbar;
