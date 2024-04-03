b036016cbd501465bca5e938cab5cd4c9c03d4f1
Dalton <daltongetbaker@gmail.com>
2024-04-02 22:31:15 -0400

    created cart.contexted 4/2

---
+++ src/context/cart.context.jsx
@@ -0,0 +1,34 @@
+import { useState, useContext, createContext, useEffect } from "react";
+import { AuthContext } from "./auth.context";
+import { get } from "../services/authService";
+
+const CartContext = createContext();
+
+function CartProvider({ children }) {
+  const [cart, setCart] = useState(null);
+
+  const { user } = useContext(AuthContext);
+  useEffect(() => {
+    const fetchCart = async () => {
+      try {
+        const response = await get("/carts");
+        setCart(response.data[0])
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    user && fetchCart();
+  }, [user]);
+
+
+  useEffect(() => {
+    console.log(cart)
+  }, [cart])
+  return (
+    <CartContext.Provider value={{ cart, setCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export { CartContext, CartProvider };
