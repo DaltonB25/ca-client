5a3b017d88ef6c74e638a67c0333b2af93c896c1
Dalton <daltongetbaker@gmail.com>
2024-04-02 22:28:33 -0400

    chnaged cartid to elemenet

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="overflow-y-hidden">
     
       <Navbar />
       <Header />
@@ -37,7 +37,7 @@ function App() {
         <Route element={<LoggedIn />}>
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/products/:productId" element={<ProductDetailsPage />} />
-        <Route path="/carts" component={<CartPage />} />
+        <Route path="/carts/:cartId" element={<CartPage />} />
         <Route path="/users/:userId" element={<UserProfilePage />} />
         </Route>
 
