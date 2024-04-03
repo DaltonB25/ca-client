28a9680f9f1141ec128e6cf18256297d5bedf5a4
Dalton <daltongetbaker@gmail.com>
2024-04-02 22:29:29 -0400

    small tailwind changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="mt-auto bg-gray-200 py-4 text-center text-gray-500 absolute bottom-0 w-full">
+    <footer className="fixed bottom-0 left-0 w-full bg-gray-200 py-4 text-center text-gray-500">
       &copy; 2024 My E-Commerce Store. All rights reserved.
     </footer>
   );
