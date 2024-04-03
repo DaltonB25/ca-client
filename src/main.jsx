4fd27ff04c5cd97d7cf85f8a0ec6a7c78ce13e7e
Dalton <daltongetbaker@gmail.com>
2024-04-02 22:28:53 -0400

    added cartprovider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 
-import { AuthProvider } from './context/auth.context.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { AuthProvider } from "./context/auth.context.jsx";
+import { CartProvider } from "./context/cart.context.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
