c6e9133a4f50e0e33008d1dfd9ddf3fff07aa4b0
Dalton <daltongetbaker@gmail.com>
2024-04-03 22:00:12 -0400

    Made some admin changes for the Navbar 4/3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,17 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react"; // <== IMPORT
 import { AuthContext } from "../context/auth.context"; // <== IMPORT
+import { CartContext } from "../context/cart.context";
 
 import cartIcon from "../images/cart.png";
+import AdminPanelPage from "../pages/AdminPanelPage";
 
 function Navbar() {
   // Subscribe to the AuthContext to gain access to
   // the values from AuthContext.Provider `value` prop
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext); // <== ADD
+  const { isLoggedIn, user, logOutUser, admin } = useContext(AuthContext); // <== ADD
+
+  const { cart } = useContext(CartContext)
 
   const getToken = () => {
     return localStorage.getItem("authToken");
@@ -24,12 +28,17 @@ function Navbar() {
         </Link>
         {getToken() && (
           <Link to="/products">
-            <button className="text-white pr-5">Products</button>
+            <button className="text-white pr-5">Shop</button>
           </Link>
         )}
-        {getToken() && (
-          <Link to="/users/:userId">
+        {getToken() && user && (
+          <Link to='/profile'>
             <button className="text-white pr-5">Profile</button>
+          </Link>
+        )}
+        {getToken() && admin && (
+          <Link to='/products/AddProduct'>
+            <button className="text-white pr-5">Add Product</button>
           </Link>
         )}
         {getToken() && (
@@ -39,22 +48,34 @@ function Navbar() {
         )}
       </div>
 
-        {getToken() ? (
+        {getToken() && cart &&
           <div className="relative">
-           <Link to="/carts/:cartId">
+           <Link to={`/carts/${cart._id}`}>
             <img src={cartIcon} alt="Cart" className="h-8 w-8 mr-3" />
            </Link>
          </div>
-         ) : (
-        <div>
-          <Link to="/signup">
-            <button className="text-white pr-5">Sign Up</button>
-          </Link>
-          <Link to="/login">
-            <button className="text-white pr-5">Login</button>
-          </Link>
-        </div>
-      )}
+        
+         
+        }
+
+         <>
+         
+            {
+                !getToken() &&
+                <div>
+                  <Link to="/signup">
+                    <button className="text-white pr-5">Sign Up</button>
+                  </Link>
+                  <Link to="/login">
+                    <button className="text-white pr-5">Login</button>
+                  </Link>
+                </div>
+
+            }
+
+         </>
+
+      
     </nav>
   );
 }
