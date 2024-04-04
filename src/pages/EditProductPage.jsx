b38fd2f7c8cefa34643f8fb79012e6d4069833f6
Dalton <daltongetbaker@gmail.com>
2024-04-04 15:29:40 -0400

    made alot of chnages but still need fixed

--- src/pages/EditProductPage.jsx
+++ src/pages/EditProductPage.jsx
@@ -1,14 +1,21 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { get, put, axiosDelete } from "../services/authService";
+import { get, put } from "../services/authService";
+import axios from "axios";
+// import { SERVER_URL } from "../services/SERVER_URL";
 
-const SERVER_URL = 'http://localhost:4000'
+const API_URL = "http://localhost:4000";
 
 function EditProductPage() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [rating, setRating] = useState("");
+  const [stock, setStock] = useState("");
+  const [brand, setBrand] = useState("");
+  const [category, setCategory] = useState("");
 
-  const [thisProduct, setThisProduct] = useState(null)
+  const [thisProduct, setThisProduct] = useState(null);
 
   const { productId } = useParams();
   const navigate = useNavigate();
@@ -19,14 +26,27 @@ function EditProductPage() {
         const oneProduct = response.data;
         setTitle(oneProduct.title);
         setDescription(oneProduct.description);
-        setThisProduct(response.data)
+        setPrice(oneProduct.price);
+        setRating(oneProduct.rating);
+        setStock(oneProduct.stock);
+        setBrand(oneProduct.brand);
+        setCategory(oneProduct.category);
+        setThisProduct(response.data);
       })
       .catch((error) => console.log(error));
   }, [productId]);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    const requestBody = { title, description };
+    const requestBody = {
+      title,
+      description,
+      price,
+      rating,
+      stock,
+      brand,
+      category,
+    };
 
     put(`/products/${productId}`, requestBody).then((response) => {
       navigate(`/products/${productId}`);
@@ -34,9 +54,9 @@ function EditProductPage() {
   };
 
   const deleteProduct = () => {
-    axiosDelete(`/${SERVER_URL}products/delete/${productId}`)
+    axios.delete(`/products/${productId}`)
       .then(() => {
-        setThisProduct(response.data)
+        setThisProduct(response.data);
 
         navigate("/products");
       })
@@ -49,147 +69,91 @@ function EditProductPage() {
         <div className="container mx-auto px-4 py-8">
           <h1 className="text-3xl font-bold mb-4">Edit Product</h1>
           <form className="card bg-white p-4" onSubmit={handleFormSubmit}>
-            {/* Title */}
             <label className="block text-gray-700">
               Title
               <input
                 type="text"
-                value={thisProduct.title}
+                value={title}
                 name="title"
                 className="w-full border border-gray-400 rounded-md p-2 focus:outline-none focus:border-black"
-                onChange={(e) =>
-                  setProduct((prev) => ({
-                    ...prev,
-                    [e.target.name]: e.target.value,
-                  }))
-                }
+                onChange={(e) => setTitle(e.target.value)}
               />
             </label>
 
-            {/* Description */}
             <label className="block text-gray-700">
               Description
               <input
                 type="text"
-                value={thisProduct.description}
+                value={description}
                 name="description"
                 className="w-full border border-gray-400 rounded-md p-2 focus:outline-none focus:border-black"
-                onChange={(e) =>
-                  setProduct((prev) => ({
-                    ...prev,
-                    [e.target.name]: e.target.value,
-                  }))
-                }
+                onChange={(e) => setDescription(e.target.value)}
               />
             </label>
 
-            {/* Price */}
-            <label className="block text-gray-700">
-              Price
-              <input
-                type="text"
-                value={thisProduct.price}
-                name="price"
+            <label className="block text-gray-700">Price:</label>
+            <input
+              type="text"
+              name="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
                 className="w-full border border-gray-400 rounded-md p-2 focus:outline-none focus:border-black"
-                onChange={(e) =>
-                  setProduct((prev) => ({
-                    ...prev,
-                    [e.target.name]: e.target.value,
-                  }))
-                }
-              />
-            </label>
+            />
 
-            {/* Rating */}
-            <label className="block text-gray-700">
-              Rating
-              <input
-                type="text"
-                value={thisProduct.rating}
-                name="rating"
+            <label className="block text-gray-700">Rating:</label>
+            <input
+              type="text"
+              name="rating"
+              value={rating}
+              onChange={(e) => setRating(e.target.value)}
                 className="w-full border border-gray-400 rounded-md p-2 focus:outline-none focus:border-black"
-                onChange={(e) =>
-                  setProduct((prev) => ({
-                    ...prev,
-                    [e.target.name]: e.target.value,
-                  }))
-                }
-              />
-            </label>
+            />
 
-            {/* Stock */}
-            <label className="block text-gray-700">
-              Stock
-              <input
-                type="text"
-                value={thisProduct.stock}
-                name="stock"
+            <label className="block text-gray-700">Stock:</label>
+            <input
+              type="text"
+              name="stock"
+              value={stock}
+              onChange={(e) => setStock(e.target.value)}
                 className="w-full border border-gray-400 rounded-md p-2 focus:outline-none focus:border-black"
-                onChange={(e) =>
-                  setProduct((prev) => ({
-                    ...prev,
-                    [e.target.name]: e.target.value,
-                  }))
-                }
-              />
-            </label>
+            />
 
-            {/* Brand */}
-            <label className="block text-gray-700">
-              Brand
-              <input
-                type="text"
-                value={thisProduct.brand}
-                name="brand"
+            <label className="block text-gray-700">Brand:</label>
+            <input
+              type="text"
+              name="brand"
+              value={brand}
+              onChange={(e) => setBrand(e.target.value)}
                 className="w-full border border-gray-400 rounded-md p-2 focus:outline-none focus:border-black"
-                onChange={(e) =>
-                  setProduct((prev) => ({
-                    ...prev,
-                    [e.target.name]: e.target.value,
-                  }))
-                }
-              />
-            </label>
+            />
 
-            {/* Category */}
-            <label className="block text-gray-700">
-              Category
-              <select
-                value={thisProduct.category}
-                name="category"
+            <label className="block text-gray-700">Category:</label>
+            <select
+              name="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
                 className="w-full border border-gray-400 rounded-md p-2 focus:outline-none focus:border-black"
-                onChange={(e) =>
-                  setProduct((prev) => ({
-                    ...prev,
-                    [e.target.name]: e.target.value,
-                  }))
-                }
-              >
-                <option value="">-- None --</option>
-                <option value="Smartphones">Smartphones</option>
-                <option value="Laptops">Laptops</option>
-                <option value="Fitness Trackers">Fitness Trackers</option>
-                <option value="Gaming Laptops">Gaming Laptops</option>
-                <option value="Smartwatches">Smartwatches</option>
-                <option value="Gaming Consoles">Gaming Consoles</option>
-                <option value="Headphones">Headphones</option>
-                <option value="Monitors">Monitors</option>
-                <option value="Televisions">Televisions</option>
-                <option value="Cameras">Cameras</option>
-                <option value="Speakers">Speakers</option>
-                <option value="Gaming Accessories">Gaming Accessories</option>
-                <option value="Tablets">Tablets</option>
-                <option value="Drones">Drones</option>
-                <option value="Graphics Cards">Graphics Cards</option>
-                <option value="Smart Speakers">Smart Speakers</option>
-                <option value="Wearable Devices">Wearable Devices</option>
-              </select>
-            </label>
+            >
+              <option value="">-- None --</option>
+              <option value="Smartphones">Smartphones</option>
+              <option value="Laptops">Laptops</option>
+              <option value="Fitness Trackers">Fitness Trackers</option>
+              <option value="Gaming Laptops">Gaming Laptops</option>
+              <option value="Smartwatches">Smartwatches</option>
+              <option value="Gaming Consoles">Gaming Consoles</option>
+              <option value="Headphones">Headphones</option>
+              <option value="Monitors">Monitors</option>
+              <option value="Televisions">Televisions</option>
+              <option value="Cameras">Cameras</option>
+              <option value="Speakers">Speakers</option>
+              <option value="Gaming Accessories">Gaming Accessories</option>
+              <option value="Tablets">Tablets</option>
+              <option value="Drones">Drones</option>
+              <option value="Graphics Cards">Graphics Cards</option>
+              <option value="Smart Speakers">Smart Speakers</option>
+              <option value="Wearable Devices">Wearable Devices</option>
+            </select>
 
-            
-
-            {/* Thumbnail */}
-            {/* Uncomment this section if you want to include a file input for thumbnail */}
             {/* <label className="block text-gray-700">
               Thumbnail&nbsp;&nbsp;
               <input
@@ -200,14 +164,18 @@ function EditProductPage() {
               />
             </label> */}
 
-            {/* Submit Button */}
             <button
               type="submit"
               className="bg-gray-600 hover:bg-gray-700 text-white font-bold mt-5 py-2 px-4 border border-black rounded-lg"
             >
-              Edit Product
+              Submit
             </button>
-            <button className="bg-gray-600 hover:bg-gray-700 text-white font-bold mt-5 ml-5 py-2 px-4 border border-black rounded-lg" onClick={deleteProduct}>Delete</button>
+            <button
+              className="bg-gray-600 hover:bg-gray-700 text-white font-bold mt-5 ml-5 py-2 px-4 border border-black rounded-lg"
+              onClick={deleteProduct}
+            >
+              Delete
+            </button>
           </form>
         </div>
       )}
