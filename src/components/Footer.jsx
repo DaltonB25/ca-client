6fb7b0a3070ce5021565f49937a8f3c25310b193
Dalton <daltongetbaker@gmail.com>
2024-04-05 12:16:47 -0400

    made MERN change and made the mt

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-full bg-gray-200 py-4 text-center text-gray-500">
-      &copy; 2024 My E-Commerce Store. All rights reserved.
+    <footer className="fixed bottom-0 left-0 w-full bg-gray-200 py-4 text-center text-gray-500 z-50">
+      &copy; MERN Full-Stack 2024 My E-Commerce Store. All rights reserved.
     </footer>
   );
 };
